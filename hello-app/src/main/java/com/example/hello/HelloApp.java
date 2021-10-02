47701ca4d08fbd3993d5bfb3fb77a4b5960eace1
Erwan Jouan <erwanjouan@yahoo.fr>
2021-10-02 15:55:15 +0200

    [24:20] We remove user defined bean, HelloService will be instanciated by autoconfiguration. App launch is ok, but it fallbacks to default prefix. Now we want to pass custol prefix to autoconfiguration.

--- hello-app/src/main/java/com/example/hello/HelloApp.java
+++ hello-app/src/main/java/com/example/hello/HelloApp.java
@@ -1,10 +1,7 @@
 package com.example.hello;
 
-import hello.ConsoleHelloService;
-import hello.HelloService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloApp {
@@ -12,9 +9,5 @@ public class HelloApp {
     public static void main(final String[] args) {
         SpringApplication.run(HelloApp.class, args);
     }
-
-    @Bean
-    public HelloService helloService() {
-        return new ConsoleHelloService("Howdy", "!");
-    }
+    
 }
