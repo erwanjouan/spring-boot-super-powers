fd853d6ac547d8b2fe0d733b0517f7c561adad05
Erwan Jouan <erwanjouan@yahoo.fr>
2021-10-02 15:41:16 +0200

    [22:46] To solve bean conflicts, we use the condition framework, if a bean is already present when the autoconfiguration is applied, then spring boot does not add the autoconfigured bean. Test defaultServiceBacksOff() passes.

--- hello-starter/src/main/java/hello/autoconfigure/HelloAutoConfiguration.java
+++ hello-starter/src/main/java/hello/autoconfigure/HelloAutoConfiguration.java
@@ -2,12 +2,15 @@ package hello.autoconfigure;
 
 import hello.ConsoleHelloService;
 import hello.HelloService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloAutoConfiguration {
+
     @Bean
+    @ConditionalOnMissingBean
     public HelloService helloService() {
         return new ConsoleHelloService();
     }
